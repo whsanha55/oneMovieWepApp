f8e72308b30fcc19e5d25b2c648d428b9819f371
USER <USER@USER-PC>
2017-12-20 20:10:16 +0900

    commit

--- oneMovieWebApp/src/controller/booking/MemberBookingCommand.java
+++ oneMovieWebApp/src/controller/booking/MemberBookingCommand.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import controller.ActionForward;
 import controller.Command;
 import domain.movie.MovieVO;
+import domain.theater.ScreenScheduleVO;
 import domain.theater.TheaterVO;
 import model.theater.DateTheaterMovieDAO;
+import model.theater.TheaterService;
 
-public class memberBookingCommand implements Command {
+public class MemberBookingCommand implements Command {
 
 	@Override
 	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp)
@@ -23,13 +25,15 @@ public class memberBookingCommand implements Command {
 		ActionForward forward = new ActionForward();
 		try {
 			//수정 필요
-			DateTheaterMovieDAO dateTheaterMovieDAO = DateTheaterMovieDAO.getInstance();
-			List<MovieVO> movieList =dateTheaterMovieDAO.selectMovieAll();
-			List<TheaterVO> theaterList = dateTheaterMovieDAO.selectTheaterAll();
+			TheaterService theaterService = TheaterService.getInstance();
 			
+			List<MovieVO> movieList =theaterService.retrieveMovieAll();
+			List<TheaterVO> theaterList = theaterService.retrieveTheaterAll();
+			List<ScreenScheduleVO> scheduleList = theaterService.retrieveDateAll();
 			req.setAttribute("movieList", movieList);
 			req.setAttribute("theaterList", theaterList);
-			forward.setPath("/memberBooking1.jsp");
+			req.setAttribute("scheduleList", scheduleList);
+			forward.setPath("/user/booking/memberBooking.jsp");
 			forward.setRedirect(false);
 			return forward;
 		} catch (Exception e) {
